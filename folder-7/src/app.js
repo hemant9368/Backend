8a0f92861273a6d0932791aa0c6ea5caa82e06c1
hemant9368 <harshitchauhanpersonal@gmail.com>
2026-08-28 01:05:28 +0530

    connected DataBase

---
+++ folder-7/src/app.js
@@ -0,0 +1,22 @@
+const express = require("express")
+
+const noteModel = require("./models/notes.model")
+
+const app = express()
+app.use(express.json())
+
+app.post("/notes", async (req,res)=>{
+    const {title, description} = req.body
+
+   const note = await noteModel.create({
+        title,description
+    })
+
+    res.status(201).json({
+        message: "note created Successfully",
+        note
+    })
+})
+
+
+module.exports = app
