172acd90e433b43efc9764ff7cb72c9137bffef0
hemant9368 <harshitchauhanpersonal@gmail.com>
2026-09-04 04:40:48 +0530

    Devlopment

---
+++ folder-9/Frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+  function fetchNotes() {
+    axios.get("http://localhost:3000/api/notes").then((res) => {
+      setNotes(res.data.Notes);
+    });
+  }
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+  return (
+    <>
+      <div className="notes">
+        {notes.map((note) => {
+          return (
+            <div className="note">
+              <h1>{note.title}</h1>
+              <p>{note.description}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+export default App;
